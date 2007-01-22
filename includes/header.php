e06313e14f7d5310212e48adf27c7caf66c18942
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2007-01-22 08:24:37 +0000

    Добавлена установка прав доступа, если функция chmod разрешена

--- includes/header.php
+++ includes/header.php
@@ -273,6 +273,11 @@ require('includes/google_conversiontracking.js.php');
   // check if the configure.php file is writeable
   if (WARN_CONFIG_WRITEABLE == 'true') {
     if ( (file_exists(dirname($_SERVER['SCRIPT_FILENAME']) . '/includes/configure.php')) && (is_writeable(dirname($_SERVER['SCRIPT_FILENAME']) . '/includes/configure.php')) ) {
+
+  // try to set permissions
+  if (function_exists('chmod')) {
+    chmod(dirname($_SERVER['SCRIPT_FILENAME']) . '/includes/configure.php', 0444);
+  }
       xtc_output_warning(WARNING_CONFIG_FILE_WRITEABLE);
     }
   }
