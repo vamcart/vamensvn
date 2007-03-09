a59e5ee31622ff9b2bfca9847c291c05f3e77525
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2007-03-09 11:27:36 +0000

    Добавлены файлы модуля Article Manager для админки

---
+++ lang/english/admin/articles_xsell.php
@@ -0,0 +1,34 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: articles_xsell.php 1125 2007-03-09 11:13:01Z VaM $   
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   --------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(configuration.php,v 1.40 2002/12/29); www.oscommerce.com 
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Cross-Sell Articles to Products');
+
+define('HEADING_ARTICLE_NAME', 'Article Name');
+define('HEADING_CROSS_ASSOCIATION', 'Associated Products');
+define('HEADING_CROSS_SELL_ACTIONS', 'Cross-Sell Actions');
+define('HEADING_PRODUCT_NAME', 'Product Name');
+define('HEADING_PRODUCT_ORDER', 'Order (1=Top)');
+
+define('TEXT_DATABASE_UPDATED', 'The database has been updated.');
+define('TEXT_NO_DELETE', 'could not delete');
+define('TEXT_NO_INSERT', 'could not insert into database');
+define('TEXT_NO_UPDATE', 'could not update database');
+
+define('TEXT_LINK_MAIN_PAGE', '<a href="%s">Click here</a> to go back to the main page.');
+define('TEXT_LINK_SORT_PRODUCTS', '<a href="%s">Click here</a> to sort the products associated with this article.<br><small>Note: this needs to be done every time a product is added or deleted.</small>');
+
+?>
