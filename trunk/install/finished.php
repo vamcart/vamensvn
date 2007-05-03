8a7e074de23a4be0579fde16397a26ddce11b895
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2007-05-03 07:37:25 +0000

    Добавлена ссылка на xt-commerce в установку

--- trunk/install/finished.php
+++ trunk/install/finished.php
@@ -156,7 +156,8 @@
 </div>
 <!-- /Контейнер -->
 
-<div id="copyright">Powered by <a href="http://vamshop.ru" target="_blank">VaM Shop</a></div>
+<div class="copyright">eCommerce Engine ©2007 <a href="http://xt-commerce.com" target="_blank">xt:Commerce</a></div>
+<div id="copyright">Powered by <a href="http://vamshop.com" target="_blank">VaM Shop</a></div>
 
 </body>
 </html>
