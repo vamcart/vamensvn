1ae55db4b10ccb6fc30c5715f72338390e82447a
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2007-10-09 11:10:21 +0000

    Корректировка

--- trunk/lang/english/english.php
+++ trunk/lang/english/english.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: english.php 1260 2007/02/07 13:24:46 VaM $
+   $Id: english.php 1260 2007/10/07 13:24:46 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
