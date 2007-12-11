a46e473989adaac9bda6e6b9cbf6502001b86862
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2007-12-11 08:23:10 +0000

    В модуль доставки самовывоз добавлена возможность указания зоны и налога

--- trunk/lang/english/modules/shipping/selfpickup.php
+++ trunk/lang/english/modules/shipping/selfpickup.php
@@ -32,6 +32,10 @@ define('MODULE_SHIPPING_SELFPICKUP_ALLOWED_TITLE' , 'Allowed Zones');
 define('MODULE_SHIPPING_SELFPICKUP_ALLOWED_DESC' , 'Please enter the zones <b>separately</b> which should be allowed to use this modul (e. g. AT,DE (leave empty if you want to allow all zones))');
 define('MODULE_SHIPPING_SELFPICKUP_STATUS_TITLE', 'Enable Self Pickup');
 define('MODULE_SHIPPING_SELFPICKUP_STATUS_DESC', 'Do you want to offer pickup by the customer?');
+define('MODULE_SHIPPING_SELFPICKUP_TAX_CLASS_TITLE' , 'Tax Class');
+define('MODULE_SHIPPING_SELFPICKUP_TAX_CLASS_DESC' , 'Use the following tax class on the shipping fee.');
+define('MODULE_SHIPPING_SELFPICKUP_ZONE_TITLE' , 'Shipping Zone');
+define('MODULE_SHIPPING_SELFPICKUP_ZONE_DESC' , 'If a zone is selected, only enable this shipping method for that zone.');
 define('MODULE_SHIPPING_SELFPICKUP_SORT_ORDER_TITLE', 'Sort Order');
 define('MODULE_SHIPPING_SELFPICKUP_SORT_ORDER_DESC', 'Sort order of display.');
 ?>
