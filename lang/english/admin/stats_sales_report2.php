2735c6dc4baf44867b77d5b97d9640473e8adce3
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2007-12-14 15:22:05 +0000

    Добавлен новый модуль статистики Админка - Разное - Статистика - Статистика продаж 2

---
+++ lang/english/admin/stats_sales_report2.php
@@ -0,0 +1,60 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: stats_sales_report2.php 1167 2007-12-14 11:13:01Z VaM $   
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   --------------------------------------------------------------
+   based on: 
+   (c) 2004	 Charly Wilhelm (stats_sales_report.php,v 0.1 2003/08/24); oscommerce.com
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Sales Report');
+
+define('TABLE_HEADING_CONVERSION', 'Total Sales');
+define('TABLE_HEADING_CONV_PER_ORDER', 'Avg. Order');
+define('TABLE_HEADING_ORDERS', '# of Orders');
+define('TEXT_MONTH', 'Month');
+define('TEXT_WEEK', 'Week');
+
+define('TABLE_HEADING_VARIANCE', '+/- %');
+
+define('AVERAGE_HOURLY_TOTAL', 'Average Hourly Total:');
+define('AVERAGE_DAILY_TOTAL', 'Average Daily Total:');
+define('AVERAGE_MONTHLY_TOTAL', 'Average Monthly Total:');
+define('AVERAGE_WEEKLY_TOTAL', 'Average Weekly Total:');
+define('AVERAGE_YEARLY_TOTAL', 'Average Yearly Total:');
+
+define('AVERAGE_ORDER', 'Average Order:');
+
+define('TODAY_TO_DATE', 'Daily Total:');
+define('WEEK_TO_DATE', 'Weekly Total:');
+define('MONTH_TO_DATE', 'Monthly Total:');
+define('YEAR_TO_DATE', 'Yearly Total:');
+define('YEARLY_TOTAL', 'Total Sales:');
+
+define('REPORT_TYPE_HOURLY', 'Hourly');
+define('REPORT_TYPE_DAILY', 'Daily');
+define('REPORT_TYPE_WEEKLY', 'Weekly');
+define('REPORT_TYPE_MONTHLY', 'Monthly');
+define('REPORT_TYPE_YEARLY', 'Yearly');
+
+define('FILTER_STATUS', 'Status');
+define('FILTER_VALUE', 'Value');
+
+define('TEXT_PREVIOUS', 'Previous');
+define('TEXT_NEXT', 'Next');
+
+define('FULL_DATE_FORMAT', 'M. d, Y');
+define('FROM_DATE_FORMAT', 'M. d');
+define('TO_DATE_FORMAT', 'd, Y');
+
+define('DAILY_TOTAL','daily total');
+define('AVERAGE_TOTAL','avg. order');
+
+?>
