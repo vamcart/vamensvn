33847241fb4668e1087970f461e0d9a33c9206a6
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2008-01-13 16:16:46 +0000

    В магазин добавлена статистика Google Analytics (Настройки в Админке - Настройки - Основные - Google Analytics

--- trunk/lang/english/admin/english.php
+++ trunk/lang/english/admin/english.php
@@ -108,7 +108,7 @@ define('BOX_CONFIGURATION_14', 'Gzip Compression');
 define('BOX_CONFIGURATION_15', 'Sessions');
 define('BOX_CONFIGURATION_16', 'Meta-Tags / Searchengines');
 define('BOX_CONFIGURATION_17', 'Specialmodules');
-define('BOX_CONFIGURATION_19', 'xt:C Partner');
+define('BOX_CONFIGURATION_19', 'Google Analytics');
 define('BOX_CONFIGURATION_22', 'Search-Options');
 define('BOX_CONFIGURATION_23', 'Yandex-Market');
 define('BOX_CONFIGURATION_24', 'Quick Price Updates');
