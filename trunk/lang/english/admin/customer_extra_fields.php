781b5520786e0cfa80d7ad0602f1de649f00b37c
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2008-06-12 15:11:47 +0000

    Добавлен новый модуль дополнительные поля покупателей

---
+++ trunk/lang/english/admin/customer_extra_fields.php
@@ -0,0 +1,41 @@
+<?php
+/*
+  $Id: extra_fields.php, v1.2 2002/08/19 01:58:58 hpdl Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Customers Extra fields');
+
+define('TABLE_HEADING_FIELDS', 'Fields');
+define('TABLE_HEADING_ACTION', 'Action');
+
+define('TEXT_HEADING_NEW_FIELD', 'New Field');
+define('TEXT_HEADING_EDIT_FIELD', 'Edit Field');
+define('TEXT_HEADING_DELETE_FIELD', 'Delete Field');
+define('TABLE_HEADING_STATUS','Status');
+
+define('TEXT_FIELD', 'Field: ');
+define('TEXT_FIELD_INPUT_TYPE', 'Input Type: ');
+define('TEXT_FIELD_REQUIRED_STATUS', 'Required: ');
+define('TEXT_FIELD_REQUIRED_EMAIL', 'Send in email: ');
+define('TEXT_FIELD_STATUS_EMAIL', 'Send in email: ');
+define('TEXT_INPUT_FIELD','Input field');
+define('TEXT_TEXTAREA_FIELD','Textarea field');
+define('TEXT_FIELD_SIZE', 'Field size: ');
+define('TEXT_NEW_INTRO', 'Add new field');
+define('TEXT_EDIT_INTRO', 'Edit field');
+define('TEXT_FIELD_NAME', 'Field name: ');
+define('TEXT_DELETE_INTRO', 'Are you sure you want to delete this field?');
+
+define('IMAGE_ICON_STATUS_GREEN', 'Active');
+define('IMAGE_ICON_STATUS_GREEN_LIGHT', 'Set Active');
+define('IMAGE_ICON_STATUS_RED', 'Inactive');
+define('IMAGE_ICON_STATUS_RED_LIGHT', 'Set Inactive');
+
+?>
