8d07b9e05a5c3c5c6dc2cc981200bb98c8789e5c
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2008-12-29 16:27:45 +0000

    Корректировка в переводе модулей оплаты

--- trunk/lang/english/modules/payment/webmoney_merchant.php
+++ trunk/lang/english/modules/payment/webmoney_merchant.php
@@ -19,29 +19,29 @@
 
   define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_TITLE', 'WebMoney Merchant');
   define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_PUBLIC_TITLE', 'WebMoney Merchant');
-  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');
+  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_DESCRIPTION', '');
   
-  define('MODULE_PAYMENT_WEBMONEYMERCHANT_TEXT_TYPE','Способ оплаты:');
+  define('MODULE_PAYMENT_WEBMONEYMERCHANT_TEXT_TYPE','Payment Type:');
   define('MODULE_PAYMENT_WEBMONEYMERCHANT_TEXT_WMZ','WMZ');
   define('MODULE_PAYMENT_WEBMONEYMERCHANT_TEXT_WMR','WMR');
 
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_STATUS_TITLE' , 'Разрешить модуль WebMoney');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_STATUS_DESC' , 'Вы хотите разрешить использование модуля при оформлении заказов?');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ALLOWED_TITLE' , 'Разрешённые страны');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ALLOWED_DESC' , 'Укажите коды стран, для которых будет доступен данный модуль (например RU,DE (оставьте поле пустым, если хотите что б модуль был доступен покупателям из любых стран))');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_STATUS_TITLE' , 'Enable WebMoney Module');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_STATUS_DESC' , 'Do you want to accept WebMoney as payments?');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ALLOWED_TITLE' , 'Allowed zones');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ALLOWED_DESC' , 'Please enter the zones <b>separately</b> which should be allowed to use this modul (e. g. AT,DE (leave empty if you want to allow all zones))');
 define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ID_TITLE' , 'WM ID:');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ID_DESC' , 'Укажите Ваш WM ID');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMZ_TITLE' , 'Ваш WMZ кошелёк:');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMZ_DESC' , 'Укажите номер Вашего WMZ кошелька');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMR_TITLE' , 'Ваш WMR кошелёк:');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMR_DESC' , 'Укажите номер Вашего WMR кошелька');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_SORT_ORDER_TITLE' , 'Порядок сортировки');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_SORT_ORDER_DESC' , 'Порядок сортировки модуля.');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ZONE_TITLE' , 'Зона');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ZONE_DESC' , 'Если выбрана зона, то данный модуль оплаты будет виден только покупателям из выбранной зоны.');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_PREPARE_ORDER_STATUS_ID_TITLE' , 'Укажите статус заказа, ожидающий оплаты');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_PREPARE_ORDER_STATUS_ID_DESC' , 'Укажите статус заказа, ожидающий оплаты.');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ORDER_STATUS_ID_TITLE' , 'Укажите оплаченный статус заказа');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ORDER_STATUS_ID_DESC' , 'Укажите оплаченный статус заказа.');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ID_DESC' , 'Your WM ID');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMZ_TITLE' , 'Your WMZ number:');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMZ_DESC' , 'Your WMZ number');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMR_TITLE' , 'Your WMR number:');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMR_DESC' , 'Your WMR number');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_SORT_ORDER_TITLE' , 'Sort order of display');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_SORT_ORDER_DESC' , 'Sort order of display. Lowest is displayed first.');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ZONE_TITLE' , 'Payment Zone');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ZONE_DESC' , 'If a zone is selected, only enable this payment method for that zone.');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_PREPARE_ORDER_STATUS_ID_TITLE' , 'Prepare order status');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_PREPARE_ORDER_STATUS_ID_DESC' , 'Prepare order status.');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ORDER_STATUS_ID_TITLE' , 'Confirmed order status');
+define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ORDER_STATUS_ID_DESC' , 'Confirmed order status.');
   
 ?>
