02da0d0b0bd48a4803423060596a7da6164c47e6
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2009-01-27 09:28:38 +0000

    Добавлены подсказки на страницу в Админке - Покупатели - Клиенты - Доступ в админку

---
+++ trunk/lang/english/admin/accounting.php
@@ -0,0 +1,108 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: accounting.php 1125 2007-03-09 11:13:01Z VaM $   
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   --------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(configuration.php,v 1.40 2002/12/29); www.oscommerce.com 
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('ACCESS_CONFIGURATION', '');
+define('ACCESS_MODULES', '');
+define('ACCESS_COUNTRIES', '');
+define('ACCESS_CURRENCIES', '');
+define('ACCESS_ZONES', '');
+define('ACCESS_GEO_ZONES', '');
+define('ACCESS_TAX_CLASSES', '');
+define('ACCESS_TAX_RATES', '');
+define('ACCESS_ACCOUNTING', '');
+define('ACCESS_BACKUP', '');
+define('ACCESS_CACHE', '');
+define('ACCESS_SERVER_INFO', '');
+define('ACCESS_WHOS_ONLINE', '');
+define('ACCESS_LANGUAGES', '');
+define('ACCESS_DEFINE_LANGUAGE', '');
+define('ACCESS_ORDERS_STATUS', '');
+define('ACCESS_SHIPPING_STATUS', '');
+define('ACCESS_MODULE_EXPORT', '');
+define('ACCESS_CUSTOMERS', '');
+define('ACCESS_CREATE_ACCOUNT', '');
+define('ACCESS_CUSTOMERS_STATUS', '');
+define('ACCESS_ORDERS', '');
+define('ACCESS_CAMPAIGNS', '');
+define('ACCESS_PRINT_PACKINGSLIP', '');
+define('ACCESS_PRINT_ORDER', '');
+define('ACCESS_POPUP_MEMO', '');
+define('ACCESS_COUPON_ADMIN', '');
+define('ACCESS_LISTCATEGORIES', '');
+define('ACCESS_GV_QUEUE', '');
+define('ACCESS_GV_MAIL', '');
+define('ACCESS_GV_SENT', '');
+define('ACCESS_VALIDPRODUCTS', '');
+define('ACCESS_VALIDCATEGORIES', '');
+define('ACCESS_MAIL', '');
+define('ACCESS_CATEGORIES', '');
+define('ACCESS_NEW_ATTRIBUTES', '');
+define('ACCESS_PRODUCTS_ATTRIBUTES', '');
+define('ACCESS_MANUFACTURERS', '');
+define('ACCESS_REVIEWS', '');
+define('ACCESS_SPECIALS', '');
+define('ACCESS_STATS_PRODUCTS_EXPECTED', '');
+define('ACCESS_STATS_PRODUCTS_VIEWED', '');
+define('ACCESS_STATS_PRODUCTS_PURCHASED', '');
+define('ACCESS_STATS_CUSTOMERS', '');
+define('ACCESS_STATS_SALES_REPORT', '');
+define('ACCESS_STATS_CAMPAIGNS', '');
+define('ACCESS_BANNER_MANAGER', '');
+define('ACCESS_BANNER_STATISTICS', '');
+define('ACCESS_MODULE_NEWSLETTER', '');
+define('ACCESS_START', '');
+define('ACCESS_CONTENT_MANAGER', '');
+define('ACCESS_CONTENT_PREVIEW', '');
+define('ACCESS_CREDITS', '');
+define('ACCESS_BLACKLIST', '');
+define('ACCESS_ORDERS_EDIT', '');
+define('ACCESS_POPUP_IMAGE', '');
+define('ACCESS_CSV_BACKEND', '');
+define('ACCESS_PRODUCTS_VPE', '');
+define('ACCESS_CROSS_SELL_GROUPS', '');
+define('ACCESS_FCK_WRAPPER', '');
+define('ACCESS_EASYPOPULATE', '');
+define('ACCESS_QUICK_UPDATES', '');
+define('ACCESS_LATEST_NEWS', '');
+define('ACCESS_RECOVER_CART_SALES', '');
+define('ACCESS_FEATURED', '');
+define('ACCESS_CIP_MANAGER', '');
+define('ACCESS_AUTHORS', '');
+define('ACCESS_ARTICLES', '');
+define('ACCESS_ARTICLES_CONFIG', '');
+define('ACCESS_STATS_SALES_REPORT2', '');
+define('ACCESS_CHART_DATA', '');
+define('ACCESS_ARTICLES_XSELL', '');
+define('ACCESS_EMAIL_MANAGER', '');
+define('ACCESS_CATEGORY_SPECIALS', '');
+define('ACCESS_PRODUCTS_OPTIONS', '');
+define('ACCESS_PRODUCT_EXTRA_FIELDS', '');
+define('ACCESS_SHIP2PAY', '');
+define('ACCESS_FAQ', '');
+define('ACCESS_AFFILIATE_AFFILIATES', '');
+define('ACCESS_AFFILIATE_BANNERS', '');
+define('ACCESS_AFFILIATE_CLICKS', '');
+define('ACCESS_AFFILIATE_CONTACT', '');
+define('ACCESS_AFFILIATE_INVOICE', '');
+define('ACCESS_AFFILIATE_PAYMENT', '');
+define('ACCESS_AFFILIATE_POPUP_IMAGE', '');
+define('ACCESS_AFFILIATE_SALES', '');
+define('ACCESS_AFFILIATE_STATISTICS', '');
+define('ACCESS_AFFILIATE_SUMMARY', '');
+define('ACCESS_CUSTOMER_EXTRA_FIELDS', '');
+
+?>
