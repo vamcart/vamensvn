681c24b47d7f51bee716cda0342d4d48b77ec7cb
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2009-09-23 15:57:01 +0000

    Корректировка кода

--- trunk/lang/english/admin/configuration.php
+++ trunk/lang/english/admin/configuration.php
@@ -309,8 +309,6 @@ define('CATEGORIES_IMAGE_THUMBNAIL_MOTION_BLUR_DESC' , 'Category Images: Motion-
 define('IMAGE_MANIPULATOR_TITLE','GDlib processing');
 define('IMAGE_MANIPULATOR_DESC','Image Manipulator for GD2 or GD1');
 
-define('ACCOUNT_LAST_NAME_TITLE' , 'Last Name');
-define('ACCOUNT_LAST_NAME_DESC' , 'Display last name in the customers account');
 define('ACCOUNT_SECOND_NAME_TITLE' , 'Second Name');
 define('ACCOUNT_SECOND_NAME_DESC' , 'Display second name in the customers account');
 define('ACCOUNT_GENDER_TITLE' , 'Gender');
