1b3176600cc6f141d316154a85b064bd71c6e39e
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2009-09-23 13:28:50 +0000

    Добавлена возомжность поиска при добавлении рекомендуемых товаров

---
+++ trunk/lang/english/admin/select_featured.php
@@ -0,0 +1,32 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: select_featured.php 899 2007-02-07 17:36:57 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   --------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(specials.php,v 1.10 2002/01/31); www.oscommerce.com 
+   (c) 2003	 nextcommerce (specials.php,v 1.4 2003/08/14); www.nextcommerce.org
+   (c) 2004	 xt:Commerce (specials.php,v 1.4 2003/08/14); xt-commerce.com
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Featured Products');
+
+define('TABLE_HEADING_PRODUCTS', 'Product Name');
+define('TABLE_HEADING_MODEL', 'Product Model');
+define('TABLE_HEADING_PRODUCTS_PRICE', 'Product Price');
+define('TABLE_HEADING_STATUS', 'Product Status');
+
+define('TEXT_FEATURED_PRODUCT', 'Product:');
+define('TEXT_FEATURED_MODEL', 'Model:');
+
+define('SUBMIT_SEARCH', 'Search');
+
+?>
