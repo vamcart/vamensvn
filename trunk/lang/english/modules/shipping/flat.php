601f44e05b0a8e3b26bb467ebf6f3bad337792ba
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2009-10-25 07:00:34 +0000

    Корректировка перевода

--- trunk/lang/english/modules/shipping/flat.php
+++ trunk/lang/english/modules/shipping/flat.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: flat.php 899 2007/02/07 13:24:46 VaM $
+   $Id: flat.php 899 2009/02/07 13:24:46 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
