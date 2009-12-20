df51a6cba4b8ceeb9808cfaa5918f291221298bb
vam <vam@067a2da0-f9ff-11de-acd2-a780054a256f>
2009-12-20 07:57:23 +0000

    Корректировка в переводе

--- trunk/lang/english/admin/coupon_admin.php
+++ trunk/lang/english/admin/coupon_admin.php
@@ -97,4 +97,8 @@ define('COUPON_USES_COUPON_HELP', 'The maximum number of times the coupon can be
 define('COUPON_USES_USER_HELP', 'Number of times a user can use the coupon, leave blank for no limit.');
 define('COUPON_PRODUCTS_HELP', 'A comma separated list of product_ids that this coupon can be used with. Leave blank for no restrictions.');
 define('COUPON_CATEGORIES_HELP', 'A comma separated list of cpaths that this coupon can be used with, leave blank for no restrictions.');
+
+define('BUTTON_SEND_MAIL','Send');
+define('ERROR_NO_COUPON_AMOUNT','No Coupon Amount');
+
 ?>
