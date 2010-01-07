0679696db809eb325bd680aa7f9ba32aeb04e353
vamshop <vamshop@067a2da0-f9ff-11de-acd2-a780054a256f>
2010-01-07 09:46:25 +0000

    Исправлена ошибка в переводе

--- lang/english/admin/new_attributes.php
+++ lang/english/admin/new_attributes.php
@@ -1,6 +1,6 @@
 <?php
 /* --------------------------------------------------------------
-   $Id: new_attributes.php 899 2007-02-07 17:36:57 VaM $
+   $Id: new_attributes.php 899 2010-02-07 17:36:57 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
