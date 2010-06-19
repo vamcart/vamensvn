426c5a8919ac7c9358f79b4e03209437656646c7
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2010-06-19 08:08:22 +0000

    Корректировка кода в модуле оплаты PayOnline System

--- trunk/lang/english/modules/payment/payonlinesystem.php
+++ trunk/lang/english/modules/payment/payonlinesystem.php
@@ -17,25 +17,23 @@
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
-define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_TITLE', 'WebMoney Merchant');
-define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_PUBLIC_TITLE', 'WebMoney Merchant');
-define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты WebMoney Merchant<br />Как правильно настроить модуль читайте <a href="http://vamshop.ru/faq.php/faq_id/70/question/Nastroika-modulya-oplaty-WebMoney-Merchant" target="_blank"><u>здесь</u></a>.');
+define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_TITLE', 'PayOnline System (Visa, Mastercard)');
+define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_PUBLIC_TITLE', 'PayOnline System (Visa, Mastercard)');
+define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты PayOnline System<br />Как правильно настроить модуль читайте <a href="http://vamshop.ru/blog/2010/06/19/подключаем-vam-shop-к-payonlinesytem-ru/" target="_blank"><u>здесь</u></a>.');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');
   
-define('MODULE_PAYMENT_PAYONLINESYSTEM_STATUS_TITLE' , 'Разрешить модуль WebMoney');
+define('MODULE_PAYMENT_PAYONLINESYSTEM_STATUS_TITLE' , 'Разрешить модуль PayOnline System');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_STATUS_DESC' , 'Вы хотите разрешить использование модуля при оформлении заказов?');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_ALLOWED_TITLE' , 'Разрешённые страны');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_ALLOWED_DESC' , 'Укажите коды стран, для которых будет доступен данный модуль (например RU,DE (оставьте поле пустым, если хотите что б модуль был доступен покупателям из любых стран))');
-define('MODULE_PAYMENT_PAYONLINESYSTEM_ID_TITLE' , 'WM ID:');
-define('MODULE_PAYMENT_PAYONLINESYSTEM_ID_DESC' , 'Укажите Ваш WM ID');
-define('MODULE_PAYMENT_PAYONLINESYSTEM_WMR_TITLE' , 'Ваш WMR кошелёк:');
-define('MODULE_PAYMENT_PAYONLINESYSTEM_WMR_DESC' , 'Укажите номер Вашего WMR кошелька');
+define('MODULE_PAYMENT_PAYONLINESYSTEM_ID_TITLE' , 'Merchant ID:');
+define('MODULE_PAYMENT_PAYONLINESYSTEM_ID_DESC' , 'Укажите Ваш Merchant ID');
+define('MODULE_PAYMENT_PAYONLINESYSTEM_SECRET_KEY_TITLE' , 'Секретный ключ');
+define('MODULE_PAYMENT_PAYONLINESYSTEM_SECRET_KEY_DESC' , 'В данной опции укажите Ваш секретный ключ.');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_SORT_ORDER_TITLE' , 'Порядок сортировки');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_SORT_ORDER_DESC' , 'Порядок сортировки модуля.');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_ZONE_TITLE' , 'Зона');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_ZONE_DESC' , 'Если выбрана зона, то данный модуль оплаты будет виден только покупателям из выбранной зоны.');
-define('MODULE_PAYMENT_PAYONLINESYSTEM_SECRET_KEY_TITLE' , 'Секретный ключ');
-define('MODULE_PAYMENT_PAYONLINESYSTEM_SECRET_KEY_DESC' , 'В данной опции укажите Ваш ключ, указанный в опции Secret Key на сайте WebMoney Merchant.');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_ORDER_STATUS_ID_TITLE' , 'Укажите оплаченный статус заказа');
 define('MODULE_PAYMENT_PAYONLINESYSTEM_ORDER_STATUS_ID_DESC' , 'Укажите оплаченный статус заказа.');
   
