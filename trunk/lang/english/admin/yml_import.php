d4625c8cb511bc677effb74a86e7c170eb68432c
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2010-08-06 11:27:02 +0000

    Корректировка перевода модуля YML импорт

---
+++ trunk/lang/english/admin/yml_import.php
@@ -0,0 +1,24 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: yml_import.php,v 1.1 2010-08-06 17:37:57 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2010 VaMSoft Ltd.
+   --------------------------------------------------------------
+   Released under the GNU General Public License
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'YML Import/Export');
+
+define('TEXT_YML_UPDATED', 'Updated ');
+define('TEXT_YML_CHANGED', 'Not changed ');
+define('TEXT_YML_ADDED','Added ');
+define('TEXT_YML_ERROR', 'File not uploaded');
+define('TEXT_YML_MAX_SIZE', 'Max file size:');
+define('TEXT_YML_IMPORT', 'Import');
+define('TEXT_YML_EXPORT', 'Export');
+
+?>
