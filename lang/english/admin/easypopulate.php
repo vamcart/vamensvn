9121abb7857962553bb427e3402bb207ab97aa7c
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2011-02-15 17:33:57 +0000

    Добавлен новый перевод для модуля Excel импорт/экспорт

--- lang/english/admin/easypopulate.php
+++ lang/english/admin/easypopulate.php
@@ -1,12 +1,12 @@
 <?php
 /* --------------------------------------------------------------
-   $Id: easypopulate.php 899 2007-02-07 17:36:57 VaM $
+   $Id: easypopulate.php 899 2011-02-07 17:36:57 oleg_vamsoft $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
    http://vamshop.com
 
-   Copyright (c) 2007 VaM Shop
+   Copyright (c) 2011 VaM Shop
    --------------------------------------------------------------
    based on: 
    (c) 2003	 osCommerce (easypopulate.php,v 1.4 2003/08/14); oscommerce.com
@@ -14,95 +14,159 @@
    Released under the GNU General Public License 
    --------------------------------------------------------------*/
 
-define('HEADING_TITLE', 'Easy Populate Configuration');
-define('EASY_VERSION_A', 'Easy Populate Advanced ');
-define('EASY_DEFAULT_LANGUAGE', '  -  Default Language ');
+define('HEADING_TITLE', 'Easy Populate');
+
+define('EASYPOPULATE_TAB_IMPORT','Import');
+define('EASYPOPULATE_TAB_IMPORT_TEMP','Import from temp dir');
+define('EASYPOPULATE_TAB_SPLIT','Split');
+define('EASYPOPULATE_TAB_EXPORT','Export');
+define('EASYPOPULATE_TAB_QUICK_LINKS','Quick Links');
+define('EASYPOPULATE_TAB_INFO','Info');
+
+define('TEXT_EASYPOPULATE_PLEASE','Please');
+define('TEXT_EASYPOPULATE_BACKUP_TEXT','backup your database');
+define('TEXT_EASYPOPULATE_BACKUP_TEXT1','before performing any Easy Populate operations!');
+
+define('TEXT_EASYPOPULATE_BACKUP_BUTTON','Quick Backup');
+
+define('EASY_UPLOAD_EP_FILE', 'Import');
+define('EASY_INSERT', 'Insert into DB');
+
+define('TEXT_EASYPOPULATE_NORMAL','Normal');
+define('TEXT_EASYPOPULATE_ADD','Add New Only');
+define('TEXT_EASYPOPULATE_UPDATE','Update Only');
+define('TEXT_EASYPOPULATE_DELETE','Delete Only');
+
+define('EASY_IMPORT_TEMP_DIR', 'Import EP file from Dir');
+define('EASY_SPLIT_EP_FILE', 'Upload and Split a EP File');
+
+define('EASYPOPULATE_BUTTON_SPLIT','Split');
+
+define('TEXT_EASYPOPULATE_EXPORT','Export EP or Froogle Products Fil');
+
+define('TEXT_EASYPOPULATE_ON_THE_FLY','Download on-the-fly');
+define('TEXT_EASYPOPULATE_CREATE_THEN_DOWNLOAD','Create then Download');
+define('TEXT_EASYPOPULATE_CREATE_IN_TEMP','Create in Temp Dir');
+define('TEXT_EASYPOPULATE_TYPE','a');
+define('TEXT_EASYPOPULATE_COMPLETE','Complete');
+define('TEXT_EASYPOPULATE_CUSTOM','Custom');
+define('TEXT_EASYPOPULATE_PRICE_QTY','Price/Qty');
+define('TEXT_EASYPOPULATE_CATEGORIES','Categories');
+define('TEXT_EASYPOPULATE_ATTRIBUTES','Attributes');
+define('TEXT_EASYPOPULATE_FROOGLE','Froogle');
+define('TEXT_EASYPOPULATE_FILE_FORMAT',' file (model number is always included).');
+
+define('TEXT_EASYPOPULATE_LABEL_NAME','name');
+define('TEXT_EASYPOPULATE_LABEL_DESC','description');
+define('TEXT_EASYPOPULATE_LABEL_URL','url');
+define('TEXT_EASYPOPULATE_LABEL_IMAGE','image');
+define('TEXT_EASYPOPULATE_LABEL_ATTRIBUTES','attributes');
+define('TEXT_EASYPOPULATE_LABEL_CATEGORIES','categories');
+define('TEXT_EASYPOPULATE_LABEL_MANUFACTURERS','manufacturer');
+define('TEXT_EASYPOPULATE_LABEL_PRICE','price');
+define('TEXT_EASYPOPULATE_LABEL_QUANTITY','quantity');
+define('TEXT_EASYPOPULATE_LABEL_WEIGHT','weight');
+define('TEXT_EASYPOPULATE_LABEL_TAX_CLASS','tax class');
+define('TEXT_EASYPOPULATE_LABEL_AVAILABLE','available');
+define('TEXT_EASYPOPULATE_LABEL_DATE_ADDED','date added');
+define('TEXT_EASYPOPULATE_LABEL_STATUS','status');
+define('TEXT_EASYPOPULATE_LABEL_SPECIALS','specials');
+define('TEXT_EASYPOPULATE_LABEL_ADD_IMAGES','add images');
+define('TEXT_EASYPOPULATE_LABEL_VENDOR','vendor');
+define('TEXT_EASYPOPULATE_LABEL_XSELL','xsell');
+
+define('TEXT_EASYPOPULATE_FILTER_BY','Filter by: ');
+define('TEXT_EASYPOPULATE_FILTER_CATEGORY','- Category -');
+define('TEXT_EASYPOPULATE_FILTER_MANUFACTURER','- Manufacturer -');
+define('TEXT_EASYPOPULATE_FILTER_STATUS','- Status -');
+define('TEXT_EASYPOPULATE_FILTER_STATUS_ACTIVE','Active');
+define('TEXT_EASYPOPULATE_FILTER_STATUS_DISABLED','Disabled');
+define('TEXT_EASYPOPULATE_BUILD_BUTTON','Build File');
+
+define('TEXT_EASYPOPULATE_QUICK_LINKS','Quick Links');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_1','Create then Download Files');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_2','Create entire file in server memory then stream download after completed.');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_3','Download <b>Complete');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_4',' w/SPPC');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_5','file to edit');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_6','file in temp dir');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_7','Download <b>Extra Fields</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_8','Download <b>Model/Price/Qty');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_9','Download <b>Model/Category');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_10','Download <b>Froogle');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_11','Download <b>Model/Attributes');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_12','file');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_13','Create Files in Temp Dir');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_14','Create entire file in server memory then save to Temp Dir after completed.');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_15','Create <b>Complete</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_16','Create <b>Model/Price/Qty</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_17','Create <b>Model/Category</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_18','Create <b>Froogle</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_19','Create <b>Model/Attributes</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_20','Temp Directory:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_21','Temp Dir is Writable');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_22','Temp Dir is NOT Writable');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_23','Magic Quotes is:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_24','register_globals is:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_25','Split files on:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_26','Model Num Size:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_27','Price with tax:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_28','Calc Precision:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_29','Replace quotes:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_30','Field separator:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_31','tab');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_32','comma');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_33','semi-colon');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_34','tilde');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_35','dash');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_36','splat');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_37','Excel safe output:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_38','Preserve tab/cr/lf:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_39','Category depth:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_40','Enable attributes:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_41','SEF Froogle URLS:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_42','on');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_43','off');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_44','records');
+
+define('EASY_ERROR_1', 'Strange but there is no default language to work... That may not happen, just in case... ');
+define('EASY_FILE_LOCATE', 'You can get your file in the Tools/Files under ');
+
+define('TEXT_EASYPOPULATE_CLEAR','Clear');
+
 define('EASY_UPLOAD_FILE', 'File uploaded. ');
 define('EASY_UPLOAD_TEMP', 'Temporary filename: ');
 define('EASY_UPLOAD_USER_FILE', 'User filename: ');
 define('EASY_SIZE', 'Size: ');
 define('EASY_FILENAME', 'Filename: ');
-define('EASY_SPLIT_DOWN', 'You can download your split files in the Tools/Files under /temp/');
-define('EASY_UPLOAD_EP_FILE', 'Upload EP File for Import');
-define('EASY_SPLIT_EP_FILE', 'Upload and Split a EP File');
-define('EASY_INSERT', 'Insert into DB');
-define('EASY_SPLIT', 'Upload and Split a EP File');
-define('EASY_LIMIT', 'Export:');
 
-define('TEXT_IMPORT_TEMP', 'Import Data from file in %s');
-define('TEXT_INSERT_INTO_DB', 'Insert into DB');
-define('TEXT_SELECT_ONE', 'Select a EP File for Import');
-define('TEXT_SPLIT_FILE', 'Select a EP File');
-define('EASY_LABEL_CREATE', 'Create an export file');
-define('EASY_LABEL_IMPORT', 'Import:');
-define('EASY_LABEL_EXPORT_CHARSET', 'Export file charset: ');
-define('EASY_LABEL_IMPORT_CHARSET', 'Import file charset: ');
-define('EASY_LABEL_CREATE_SELECT', 'Select method to save export file');
-define('EASY_LABEL_CREATE_SAVE', 'Save to temp file on server');
-define('EASY_LABEL_SELECT_DOWN', 'Select field set to download');
-define('EASY_LABEL_SORT', 'Select field for sort order');
-define('EASY_LABEL_PRODUCT_RANGE', 'Limit by Products_ID(s)');
-define('EASY_LABEL_LIMIT_CAT', 'Limit By Category');
-define('EASY_LABEL_LIMIT_MAN', 'Limit By Manufacturer');
+define('EASY_ERROR_7', 'Can not open file for reading. Script will terminate.<br />');
 
-define('EASY_LABEL_PRODUCT_AVAIL', 'Range Available: ');
-define('EASY_LABEL_PRODUCT_FROM', ' from ');
-define('EASY_LABEL_PRODUCT_TO', ' to ');
-define('EASY_LABEL_PRODUCT_RECORDS', '    Total number of records: ');
-define('EASY_LABEL_PRODUCT_BEGIN', 'begin: ');
-define('EASY_LABEL_PRODUCT_END', 'end: ');
-define('EASY_LABEL_PRODUCT_START', 'Start File Creation ');
+define('EASY_LABEL_FILE_COUNT_1', 'Creating file EP_Split');
 
-define('EASY_FILE_LOCATE', 'You can get your file in the Tools/Files under ');
-define('EASY_FILE_LOCATE_2', ' by clicking this Link and going to the file manager');
-define('EASY_FILE_RETURN', ' You can return to EP by clicking this link.');
-define('EASY_IMPORT_TEMP_DIR', 'Import from Temp Dir ');
-define('EASY_LABEL_DOWNLOAD', 'Download');
-define('EASY_LABEL_COMPLETE', 'Complete');
-define('EASY_LABEL_TAB', 'tab-delimited .txt file to edit');
-define('EASY_LABEL_MPQ', 'Model/Image/Name/Price/Qty');
-define('EASY_LABEL_EP_MC', 'Model/Category');
-define('EASY_LABEL_EP_FROGGLE', 'Froogle');
-define('EASY_LABEL_EP_ATTRIB', 'Attributes');
-define('EASY_LABEL_EXTRA_FIELDS', 'Product Extra Fields');
-define('EASY_LABEL_NONE', 'None');
-define('EASY_LABEL_CATEGORY', '1st Category Name');
-define('PULL_DOWN_MANUFACTURES', 'Manufacturers');
-define('EASY_LABEL_PRODUCT', 'Product ID Number');
-define('EASY_LABEL_MANUFACTURE', 'Manufacturer ID Number');
-define('EASY_LABEL_EP_FROGGLE_HEADER', 'Download a EP or Froogle file');
-define('EASY_LABEL_EP_MA', 'Model/Attributes');
-define('EASY_LABEL_EP_FR_TITLE', 'Create EP or Froogle Files in Temp Dir ');
-define('EASY_LABEL_EP_DOWN_TAB', 'Create <b>Complete</b> tab-delimited .txt file in temp dir');
-define('EASY_LABEL_EP_DOWN_MPQ', 'Create <b>Model/Price/Qty</b> tab-delimited .txt file in temp dir');
-define('EASY_LABEL_EP_DOWN_MC', 'Create <b>Model/Category</b> tab-delimited .txt file in temp dir');
-define('EASY_LABEL_EP_DOWN_MA', 'Create <b>Model/Attributes</b> tab-delimited .txt file in temp dir');
-define('EASY_LABEL_EP_DOWN_FROOGLE', 'Create <b>Froogle</b> tab-delimited .txt file in temp dir');
+define('EASY_LABEL_LINE_COUNT_1', 'Added ');
+define('EASY_LABEL_LINE_COUNT_2', 'records and closing file... ');
 
-define('EASY_LABEL_NEW_PRODUCT', '!New Product!</font><br>');
+define('EASY_SPLIT_DOWN', 'You can download your split files in the Tools/Files under');
+
+define('EASY_TEXT_DELETED', 'Deleted product ');
+define('EASY_TEXT_DELETED1', ' from the database');
+define('EASY_TEXT_NOT_DELETE', 'Did not delete ');
+define('EASY_TEXT_NOT_DELETE1', ' since it is not unique');
+
+define('EASY_ERROR_2', '... ERROR! - Too many characters in the model number.<br>
+			25 is the maximum on a standard install.<br>
+			Your maximum product_model length is set to ');
+define('EASY_ERROR_2A', ' <br>You can either shorten your model numbers or increase the size of the field in the database. Then change the setting at the top of the easypopulate.php file.</font>');
+
+define('EASY_LABEL_NEW_PRODUCT', "<font color='green'> !New Product!</font><br>");
 define('EASY_LABEL_UPDATED', "<font color='black'> Updated</font><br>");
 define('EASY_LABEL_DELETE_STATUS_1', "<font color='red'> !!Deleting product ");
 define('EASY_LABEL_DELETE_STATUS_2', " from the database !!</font><br>");
-define('EASY_LABEL_LINE_COUNT_1', 'Added ');
-define('EASY_LABEL_LINE_COUNT_2', 'records and closing file... ');
-define('EASY_LABEL_FILE_COUNT_1', 'Creating file EP_Split ');
-define('EASY_LABEL_FILE_COUNT_2', '.txt ...  ');
-define('EASY_LABEL_FILE_CLOSE_1', 'Added ');
-define('EASY_LABEL_FILE_CLOSE_2', ' records and closing file...');
-//errormessages
-define('EASY_ERROR_1', 'Strange but there is no default language to work... That may not happen, just in case... ');
-define('EASY_ERROR_2', '... ERROR! - Too many characters in the model number.<br>
-			25 is the maximum on a standard cre install.<br>
-			Your maximum product_model length is set to ');
-define('EASY_ERROR_2A', ' <br>You can either shorten your model numbers or increase the size of the field in the database.</font>');
-define('EASY_ERROR_2B',  "<font color='red'>");
-define('EASY_ERROR_3', '<p class=smallText>No products_id field in record. This line was not imported <br><br>');
-define('EASY_ERROR_4', '<font color=red>ERROR - v_customer_group_id and v_customer_price must occur in pairs</font>');
-define('EASY_ERROR_5', '</b><font color=red>ERROR - You are trying to use a file created with EP Advanced, please try with Easy Populate Advanced </font>');
-define('EASY_ERROR_5a', '<font color=red><b><u>  Click here to return to Easy Populate Basic </u></b></font>');
-define('EASY_ERROR_6', '</b><font color=red>ERROR - You are trying to use a file created with EP Basic, please try with Easy Populate Basic </font>');
-define('EASY_ERROR_6a', '<font color=red><b><u>  Click here to return to Easy Populate Advanced </u></b></font>');
 
-define('EASY_LABEL_FILE_COUNT_1A', 'Create EPA_Split file ');
+define('EASY_LABEL_TEXT_NO_MODEL', 'No products_model field in record. This line was not imported: ');
+
+define('EASY_EXTRA_FIELD_UPDATED', 'Product Extra Fields Updated');
+define('EASY_EXTRA_FIELD_ADDED', 'Product Extra Fields Inserted');
 
 ?>
