433b705e8c8b1ac07b588717f740478d6c31ac1b
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2011-04-12 06:42:14 +0000

    Добавлены новые файлы для модули pin коды

---
+++ trunk/lang/english/admin/pin_loader.php
@@ -0,0 +1,12 @@
+<?php
+
+define('HEADING_TITLE','PIN codes management');
+define('PINS_ADDED',' codes added for product:');
+define('PINS_QUANTIY_UPDATED','Products quantity updated.');
+define('PIN_HEADING_PRODUCT','Product name');
+define('PIN_HEADING_NUMBEROF', 'PINs');
+
+define('PIN_HEADING_ACTION', 'Action');
+define('PIN_ACTION_EDIT', 'Edit');
+
+?>
