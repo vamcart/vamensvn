0c754724e8a68da93495ae50ead473e37f36122a
vamshop <vamshop@067a2da0-f9ff-11de-acd2-a780054a256f>
2011-08-04 13:33:44 +0000

    Корректировка кода для нового редактора заказов

--- admin/includes/application_top.php
+++ admin/includes/application_top.php
@@ -605,8 +605,13 @@ define('TABLE_SPSR_ZONES', 'spsr_zones');
   
 define('FILENAME_CUSTOMERS_EXPORT', 'customer_export.php');
 define('FILENAME_EXPORTORDERS', 'exportorders.php');
-  
+
 define('TABLE_PRODUCTS_PINS', 'products_pins');
 define('FILENAME_PIN_LOADER', 'pin_loader.php');
 
+define('ORDER_EDITOR_PAYMENT_DROPDOWN', 'true');
+define('ORDER_EDITOR_USE_SPPC', 'false');
+define('ORDER_EDITOR_USE_AJAX', 'true');
+define('ORDER_EDITOR_CREDIT_CARD', 'cc');
+  
 ?>
