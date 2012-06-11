861d32451c7aa50c61c57681af2ff17f95332b9d
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2012-06-11 14:00:05 +0000

    Корректировка перевода

--- trunk/lang/english/english.php
+++ trunk/lang/english/english.php
@@ -650,4 +650,6 @@ define('BUTTON_PRINT_SCHET', 'Print Invoice');
 define('BUTTON_PRINT_PACKINGSLIP', 'Print Packingslip');
 define('BUTTON_PRINT_KVITANCIA', 'Print Kvitancia');
 
+define('TEXT_NO_COMPARISON_AVAILABLE', 'No Comparison Available.');
+
 ?>
