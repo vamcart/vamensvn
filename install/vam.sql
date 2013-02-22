512a402394ea5aba873f0c85a766b293c1084e3c
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2013-02-22 19:04:33 +0000

    Корректировка базы

--- install/vam.sql
+++ install/vam.sql
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------------------
-#  $Id: vamshop.sql,v 1.62 2012/11/07 20:24:17 VaM Exp $
+#  $Id: vamshop.sql,v 1.62 2012/11/08 20:24:17 VaM Exp $
 #
 #  VamShop - open source ecommerce solution
 #  http://vamshop.com 
