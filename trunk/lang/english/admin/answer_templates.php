e9da3ee1a3c55b36b3226a9011fc30ef48245cfa
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2013-05-23 15:44:19 +0000

    Добавлены файлы для нового модуля шаблоны ответа

---
+++ trunk/lang/english/admin/answer_templates.php
@@ -0,0 +1,47 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: latest_news.php 899 2007-02-07 17:36:57 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   --------------------------------------------------------------
+   based on: 
+   (c) 2003	 osCommerce (latest_news.php,v 1.4 2003/08/14); oscommerce.com
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'News');
+
+define('TABLE_HEADING_LATEST_NEWS_HEADLINE', 'Headline');
+define('TABLE_HEADING_LATEST_NEWS_ACTION', 'Action');
+define('TABLE_HEADING_LATEST_NEWS_STATUS', 'Status');
+
+define('TEXT_NEWS_ITEMS', 'Number of news:');
+define('TEXT_INFO_HEADING_DELETE_ITEM', 'Delete news');
+define('TEXT_DELETE_ITEM_INTRO', 'Are you sure?');
+
+define('TEXT_LATEST_NEWS_HEADLINE', 'Headline');
+define('TEXT_LATEST_NEWS_CONTENT', 'Content');
+
+define('IMAGE_NEW_NEWS_ITEM', 'Add news');
+define('IMAGE_EDIT_NEWS_ITEM', 'Edit news');
+define('TEXT_LATEST_NEWS_LANGUAGE', 'Language');
+define('TEXT_LATEST_NEWS_DATE', 'Date');
+
+// Сборка VaM
+
+define('IMAGE_ICON_STATUS_GREEN', 'Active');
+define('IMAGE_ICON_STATUS_GREEN_LIGHT', 'Set Active');
+define('IMAGE_ICON_STATUS_RED', 'Inactive');
+define('IMAGE_ICON_STATUS_RED_LIGHT', 'Set Inactive');
+
+define('EMPTY_CATEGORY', 'No news');
+define('TEXT_NO_CHILD_CATEGORIES_OR_PRODUCTS', 'No news at this moment');
+
+define('TEXT_NEWS_PAGE_URL', 'News SEO URL');
+
+?>
