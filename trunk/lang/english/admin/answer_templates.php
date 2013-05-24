f831aad3b75f9e36864404a040d2c01e0369baa8
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2013-05-24 08:19:16 +0000

    Корректировка перевода

--- trunk/lang/english/admin/answer_templates.php
+++ trunk/lang/english/admin/answer_templates.php
@@ -1,6 +1,6 @@
 <?php
 /* --------------------------------------------------------------
-   $Id: latest_news.php 899 2007-02-07 17:36:57 VaM $
+   $Id: latest_news.php 899 2013-02-07 17:36:57 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
