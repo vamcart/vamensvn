7c75bc54dc0537111bc96ca098748be213e672b1
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2014-01-22 09:26:38 +0000

    Корректировка базы данных по умолчанию

--- install/vam.sql
+++ install/vam.sql
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------------------
-#  $Id: vamshop.sql,v 1.62 2012/11/07 20:24:17 VaM Exp $
+#  $Id: vamshop.sql,v 1.62 2014/11/07 20:24:17 VaM Exp $
 #
 #  VamShop - open source ecommerce solution
 #  http://vamshop.com 
@@ -1272,15 +1272,15 @@ CREATE TABLE products_description (
 ) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 
 INSERT INTO `products_description` VALUES
-(1, 1, 'Samsung ATIV Book 9', 'Product description.', 'Product description.', '', '', '', '', '', 0),
-(2, 1, 'Samsung ATIV Smart PC', 'Product description.', 'Product description.', '', '', '', '', '', 0),
-(3, 1, 'Samsung ATIV Book 4', 'Product description.', 'Product description.', '', '', '', '', '', 9),
-(4, 1, 'Samsung GALAXY Tab 3', 'Product description.', 'Product description.', '', '', '', '', '', 0),
-(5, 1, 'Samsung GALAXY Note 10.1', 'Product description.', 'Product description.', '', '', '', '', '', 0),
-(6, 1, 'Samsung GALAXY Note 8', 'Product description.', 'Product description.', '', '', '', '', '', 0),
-(7, 1, 'Samsung GALAXY Note 3', 'Product description.', 'Product description.', '', '', '', '', '', 1),
-(8, 1, 'Samsung GALAXY S4', 'Product description.', 'Product description.', '', '', '', '', '', 0),
-(9, 1, 'Samsung GALAXY Ace 3', 'Product description.', 'Product description.', '', '', '', '', '', 0);
+(1, 1, 'Samsung ATIV Book 9', 'Product description.', 'Product short description.', '', '', '', '', '', 0),
+(2, 1, 'Samsung ATIV Smart PC', 'Product description.', 'Product short description.', '', '', '', '', '', 0),
+(3, 1, 'Samsung ATIV Book 4', 'Product description.', 'Product short description.', '', '', '', '', '', 9),
+(4, 1, 'Samsung GALAXY Tab 3', 'Product description.', 'Product short description.', '', '', '', '', '', 0),
+(5, 1, 'Samsung GALAXY Note 10.1', 'Product description.', 'Product short description.', '', '', '', '', '', 0),
+(6, 1, 'Samsung GALAXY Note 8', 'Product description.', 'Product short description.', '', '', '', '', '', 0),
+(7, 1, 'Samsung GALAXY Note 3', 'Product description.', 'Product short description.', '', '', '', '', '', 1),
+(8, 1, 'Samsung GALAXY S4', 'Product description.', 'Product short description.', '', '', '', '', '', 0),
+(9, 1, 'Samsung GALAXY Ace 3', 'Product description.', 'Product short description.', '', '', '', '', '', 0);
 
 DROP TABLE IF EXISTS products_pins;
 CREATE TABLE products_pins (
