01b756ebc7e9df0a2574593158efe6cf496e5e6e
vamshop@gmail.com <vamshop@gmail.com@067a2da0-f9ff-11de-acd2-a780054a256f>
2014-12-13 14:46:03 +0000

    Добавлены новые файлы в админке для модуля ярылки товара

---
+++ trunk/lang/english/admin/product_labels.php
@@ -0,0 +1,62 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: latest_news.php 899 2013-02-07 17:36:57 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   --------------------------------------------------------------
+   based on: 
+   (c) 2003	 osCommerce (latest_news.php,v 1.4 2003/08/14); oscommerce.com
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Answer Templates');
+
+define('TABLE_HEADING_LATEST_NEWS_NAME', 'Name');
+define('TABLE_HEADING_LATEST_NEWS_ACTION', 'Action');
+define('TABLE_HEADING_LATEST_NEWS_STATUS', 'Status');
+
+define('TEXT_NEWS_ITEMS', 'Number of templates:');
+define('TEXT_INFO_HEADING_DELETE_ITEM', 'Delete template');
+define('TEXT_DELETE_ITEM_INTRO', 'Are you sure?');
+
+define('TEXT_ANSWER_TEMPLATES_NAME', 'Name');
+define('TEXT_ANSWER_TEMPLATES_CONTENT', 'Content');
+
+define('IMAGE_NEW_NEWS_ITEM', 'Add template');
+define('IMAGE_EDIT_NEWS_ITEM', 'Edit template');
+define('TEXT_ANSWER_TEMPLATES_LANGUAGE', 'Language');
+define('TEXT_ANSWER_TEMPLATES_DATE', 'Date');
+
+// Сборка VaM
+
+define('IMAGE_ICON_STATUS_GREEN', 'Active');
+define('IMAGE_ICON_STATUS_GREEN_LIGHT', 'Set Active');
+define('IMAGE_ICON_STATUS_RED', 'Inactive');
+define('IMAGE_ICON_STATUS_RED_LIGHT', 'Set Inactive');
+
+define('EMPTY_CATEGORY', 'No templates');
+define('TEXT_NO_CHILD_CATEGORIES_OR_PRODUCTS', 'No templates at this moment');
+
+define('TEXT_AVAILABLE_PLACEHOLDERS', 'Available Placeholders:');
+define('TEXT_NAME', '{$NAME} - Customers name.');
+define('TEXT_ORDER_NR', '{$ORDER_NR} - Order number.');
+define('TEXT_ORDER_LINK', '{$ORDER_LINK} - Order link.');
+define('TEXT_ORDER_DATE', '{$ORDER_DATE} - Order date.');
+define('TEXT_ORDER_STATUS', '{$ORDER_STATUS} - Order Status.');
+define('TEXT_DELIVERY_NAME', '{$DELIVERY_NAME} - Delivery Name.');
+define('TEXT_DELIVERY_STREET_ADDRESS', '{$DELIVERY_STREET_ADDRESS} - Delivery Street Address.');
+define('TEXT_DELIVERY_CITY', '{$DELIVERY_CITY} - Delivery City.');
+define('TEXT_DELIVERY_POSTCODE', '{$DELIVERY_POSTCODE} - Delivery Postcode.');
+define('TEXT_DELIVERY_STATE', '{$DELIVERY_STATE} - Delivery State.');
+define('TEXT_DELIVERY_COUNTRY', '{$DELIVERY_COUNTRY} - Delivery Country.');
+define('TEXT_CUSTOMERS_TELEPHONE', '{$CUSTOMERS_TELEPHONE} - Customers telephone.');
+define('TEXT_CUSTOMERS_EMAIL_ADDRESS', '{$CUSTOMERS_EMAIL_ADDRESS} - Customers Email.');
+define('TEXT_PAYMENT_METHOD', '{$PAYMENT_METHOD} - Payment Method.');
+define('TEXT_SHIPPING_METHOD', '{$SHIPPING_METHOD} - Shipping Method.');
+
+?>
