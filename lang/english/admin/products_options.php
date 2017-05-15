0e027be36fb7b904fc711974b3262bdbd41f2989
vam <vamshop@gmail.com>
2017-05-15 10:12:09 +0300

    Добавлена возоможность сортировки атрибутов, с помощью поля порядок сортировки

--- lang/english/admin/products_options.php
+++ lang/english/admin/products_options.php
@@ -17,48 +17,49 @@
    Released under the GNU General Public License 
    --------------------------------------------------------------*/
 
-define('HEADING_TITLE_OPT', 'Product Options');
-define('HEADING_TITLE_VAL', 'Option Names');
-define('HEADING_TITLE_ATRIB', 'Products Attributes');
+define('HEADING_TITLE_OPT', 'Атрибуты товаров');
+define('HEADING_TITLE_VAL', 'Названия атрибутов');
+define('HEADING_TITLE_ATRIB', 'Атрибуты товаров');
 
 define('TABLE_HEADING_ID', 'ID');
-define('TABLE_HEADING_PRODUCT', 'Product Name');
-define('TABLE_HEADING_OPT_NAME', 'Option Name');
-define('TABLE_HEADING_OPT_VALUE', 'Option Value');
-define('TABLE_HEADING_OPT_PRICE', 'Value Price');
-define('TABLE_HEADING_OPT_PRICE_PREFIX', 'Prefix (+/-)');
-define('TABLE_HEADING_ACTION', 'Action');
-define('TABLE_HEADING_DOWNLOAD', 'Downloadable products:');
-define('TABLE_TEXT_FILENAME', 'Filename:');
-define('TABLE_TEXT_MAX_DAYS', 'Expiry days:');
-define('TABLE_TEXT_MAX_COUNT', 'Maximum download count:');
+define('TABLE_HEADING_PRODUCT', 'Название товара');
+define('TABLE_HEADING_OPT_NAME', 'Название атрибута');
+define('TABLE_HEADING_OPT_VALUE', 'Значение атрибута');
+define('TABLE_HEADING_OPT_PRICE', 'Цена');
+define('TABLE_HEADING_OPT_PRICE_PREFIX', 'Префикс');
+define('TABLE_HEADING_ACTION', 'Действие');
+define('TABLE_HEADING_DOWNLOAD', 'Скачиваемые товары:');
+define('TABLE_TEXT_FILENAME', 'Имя файла:');
+define('TABLE_TEXT_MAX_DAYS', 'Дата окончания:');
+define('TABLE_TEXT_MAX_COUNT', 'Максимум загрузок:');
+define('TABLE_TEXT_SORT_ORDER', 'Порядок сортировки');
 
 define('MAX_ROW_LISTS_OPTIONS', 10);
 
-define('TEXT_WARNING_OF_DELETE', 'This option has products and values linked to it - it is not safe to delete it.');
-define('TEXT_OK_TO_DELETE', 'This option has no products and values linked to it - it is safe to delete it.');
-define('TEXT_SEARCH','Search: ');
-define('TEXT_OPTION_ID', 'Option ID');
-define('TEXT_OPTION_NAME', 'Option Name');
+define('TEXT_WARNING_OF_DELETE', 'Эта опция связана с товарами и не сохранится после ее удаления.');
+define('TEXT_OK_TO_DELETE', 'Эта опция не связана с товарами и не сохранится после ее удаления.');
+define('TEXT_SEARCH','Поиск: ');
+define('TEXT_OPTION_ID', 'Код атрибута');
+define('TEXT_OPTION_NAME', 'Название атрибута');
 
 // VaM
 
-define('TABLE_HEADING_OPT_IMAGE','Image');
-define('TABLE_HEADING_OPT_DESC','Description');
-define('TABLE_TEXT_DELETE','Delete image');
-define('TEXT_OPTIONS_IMAGE','Option image');
-define('TEXT_NOTE','*NOTE: Rows / Size / Length only for TEXT attributes');
-define('TEXT_ROWS','Rows');
-define('TEXT_SIZE','Size');
-define('TEXT_MAX_LENGTH','Max Length');
-define('TABLE_HEADING_OPT_TYPE','Option Type');
+define('TABLE_HEADING_OPT_IMAGE','Картинка');
+define('TABLE_HEADING_OPT_DESC','Описание');
+define('TABLE_TEXT_DELETE','Удалить картинку');
+define('TEXT_OPTIONS_IMAGE','Картинка атрибута');
+define('TEXT_NOTE','*ЗАМЕЧАНИЕ: Опции Ряды / Размер / Длина действительны только для атрибутов типа TEXT');
+define('TEXT_ROWS','Ряды');
+define('TEXT_SIZE','Размер');
+define('TEXT_MAX_LENGTH','Длина');
+define('TABLE_HEADING_OPT_TYPE','Тип атрибута');
 
-define('TEXT_TYPE_SELECT','-- Please Select --');
+define('TEXT_TYPE_SELECT','-- Выберите --');
 define('TEXT_TYPE_DROPDOWN','Dropdown');
 define('TEXT_TYPE_TEXT','Text');
 define('TEXT_TYPE_TEXTAREA','Textarea');
 define('TEXT_TYPE_RADIO','Radio');
 define('TEXT_TYPE_CHECKBOX','Checkbox');
-define('TEXT_TYPE_READ_ONLY','Read Only');
+define('TEXT_TYPE_READ_ONLY','Только для чтения');
 
 ?>
