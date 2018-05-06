d15074be78d684fbf256dd6b219ddac11abbe794
vam <vamshop@gmail.com>
2018-05-06 12:29:56 +0300

    Добавлена возмжность добавления аватарки авторам статей

--- lang/english/admin/authors.php
+++ lang/english/admin/authors.php
@@ -15,36 +15,36 @@
    Released under the GNU General Public License 
    --------------------------------------------------------------*/
 
-define('HEADING_TITLE', 'Authors');
+define('HEADING_TITLE', 'Авторы');
 
-define('TABLE_HEADING_AUTHORS', 'Authors');
-define('TABLE_HEADING_ACTION', 'Action');
+define('TABLE_HEADING_AUTHORS', 'Авторы');
+define('TABLE_HEADING_ACTION', 'Действие');
 
-define('TEXT_HEADING_NEW_AUTHOR', 'New Author');
-define('TEXT_HEADING_EDIT_AUTHOR', 'Edit Author');
-define('TEXT_HEADING_DELETE_AUTHOR', 'Delete Author');
+define('TEXT_HEADING_NEW_AUTHOR', 'Добавить автора');
+define('TEXT_HEADING_EDIT_AUTHOR', 'Редактировать автора');
+define('TEXT_HEADING_DELETE_AUTHOR', 'Удалить автора');
 
-define('TEXT_ARTICLE_BY', 'Article by ');
-define('TEXT_MORE_INFORMATION', 'For more information, please visit this authors <a href="http://%s" target="_blank">web page</a>.');
+define('TEXT_ARTICLE_BY', 'Автор статьи: ');
+define('TEXT_MORE_INFORMATION', 'Чтобы узнать больше, зайдите <a href="http://%s" target="_blank">сюда</a>.');
 
-define('TEXT_AUTHORS', 'Authors:');
-define('TEXT_AUTHORS_IMAGE', 'Image:');
-define('TEXT_DATE_ADDED', 'Date Added:');
-define('TEXT_LAST_MODIFIED', 'Last Modified:');
-define('TEXT_ARTICLES', 'Articles:');
+define('TEXT_AUTHORS', 'Авторы:');
+define('TEXT_AUTHORS_IMAGE', 'Картинка:');
+define('TEXT_DATE_ADDED', 'Дата добавления:');
+define('TEXT_LAST_MODIFIED', 'Последнее изменение:');
+define('TEXT_ARTICLES', 'Статьи:');
 
-define('TEXT_NEW_INTRO', 'Please fill out the following information for the new author');
-define('TEXT_EDIT_INTRO', 'Please make any necessary changes.');
+define('TEXT_NEW_INTRO', 'Заполните данную форму');
+define('TEXT_EDIT_INTRO', 'Внесите необходимые изменения.');
 
-define('TEXT_AUTHORS_NAME', 'Authors Name:');
-define('TEXT_AUTHORS_DESCRIPTION', 'Authors Description:');
-define('TEXT_AUTHORS_URL', 'Authors URL (without http://):');
+define('TEXT_AUTHORS_NAME', 'Имя автора:');
+define('TEXT_AUTHORS_DESCRIPTION', 'Описание:');
+define('TEXT_AUTHORS_URL', 'URL ссылка (без http://):');
 
-define('TEXT_DELETE_INTRO', 'Are you sure you want to delete this author?');
-define('TEXT_DELETE_ARTICLES', 'Delete articles from this author?');
-define('TEXT_DELETE_WARNING_ARTICLES', '<b>WARNING:</b> There are %s articles still linked to this author!');
+define('TEXT_DELETE_INTRO', 'Вы действительно хотите удалить данного автора?');
+define('TEXT_DELETE_ARTICLES', 'Удалить статьи данного автора?');
+define('TEXT_DELETE_WARNING_ARTICLES', '<b>Внимание:</b> Имеется %s статей данного автора!');
 
-define('ERROR_DIRECTORY_NOT_WRITEABLE', 'Error: I can not write to this directory. Please set the right user permissions on: %s');
-define('ERROR_DIRECTORY_DOES_NOT_EXIST', 'Error: Directory does not exist: %s');
+define('ERROR_DIRECTORY_NOT_WRITEABLE', 'Ошибка: Каталог защищён от записи, установите необходимые права доступа: %s');
+define('ERROR_DIRECTORY_DOES_NOT_EXIST', 'Ошибка: Каталог отсутствует: %s');
 
 ?>
