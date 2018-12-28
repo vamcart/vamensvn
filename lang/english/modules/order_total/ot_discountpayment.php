ab9b54d167e4c62e9f04a6a5f7d1ca6c8dd25d6b
vam <vamshop@gmail.com>
2018-12-28 20:41:50 +0300

    Добавлен новый модуль - Скидка на выбраный модуль оплаты

---
+++ lang/english/modules/order_total/ot_discountpayment.php
@@ -0,0 +1,30 @@
+<?php
+/*
+  $Id: ot_discountpayment.php,v 1.0 2003/6/19 23:09:49 wib Exp $
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+  Copyright (c) 2008 osCommerce
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TITLE', 'Payment Type Discount');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_DESCRIPTION', 'Payment Type Discount');
+
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_STATUS_TITLE','Display Total');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_STATUS_DESC','Do you want to enable the Order Payment Discount?');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_SORT_ORDER_TITLE','Sort Order');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_SORT_ORDER_DESC','Sort order of display.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_SHIPPING_TITLE','Include Shipping');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_SHIPPING_DESC','Include Shipping in calculation');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_TAX_TITLE','Include Tax');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_TAX_DESC','Include Tax in calculation.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_PERCENTAGE_TITLE','Discount Percentage');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_PERCENTAGE_DESC','Amount of Discount(percentage).');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_CALC_TAX_TITLE','Calculate Tax');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_CALC_TAX_DESC','Re-calculate Tax on discount amount.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_MINIMUM_TITLE','Minimum Amount');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_MINIMUM_DESC','Minimum order before discount');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TYPE_TITLE','Payment Type');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TYPE_DESC','Payment Type to discount');
+  
+?>
