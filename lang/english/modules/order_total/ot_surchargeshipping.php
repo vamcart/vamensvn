3de2e895e3ed39d5b6f90e2d5b654036e2d2a038
vam <vamshop@gmail.com>
2018-12-28 20:45:11 +0300

    Добавлен новый модуль - Наценка на выбраный способ доставки

---
+++ lang/english/modules/order_total/ot_surchargeshipping.php
@@ -0,0 +1,30 @@
+<?php
+/*
+  $Id: ot_surchargeshipping.php,v 1.0 2003/6/19 23:09:49 wib Exp $
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+  Copyright (c) 2008 osCommerce
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_TITLE', 'Shipping Type Surcharge');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_DESCRIPTION', 'Shipping Type Fee');
+
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_STATUS_TITLE','Display Total');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_STATUS_DESC','Do you want to enable the Order Shipping Fee?');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_SORT_ORDER_TITLE','Sort Order');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_SORT_ORDER_DESC','Sort order of display.');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_INC_SHIPPING_TITLE','Include Shipping');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_INC_SHIPPING_DESC','Include Shipping in calculation');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_INC_TAX_TITLE','Include Tax');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_INC_TAX_DESC','Include Tax in calculation.');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_PERCENTAGE_TITLE','Surcharge Percentage');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_PERCENTAGE_DESC','Amount of Surcharge(percentage).');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_CALC_TAX_TITLE','Calculate Tax');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_CALC_TAX_DESC','Re-calculate Tax on surcharged amount.');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_MINIMUM_TITLE','Minimum Amount');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_MINIMUM_DESC','Minimum order before fee');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_TYPE_TITLE','Shipping Type');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_TYPE_DESC','Shipping Type to pay surcharge');
+  
+?>
