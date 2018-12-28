f84fcffa27b4cadc1e1bb54d338f03c6353b7393
vam <vamshop@gmail.com>
2018-12-28 20:44:19 +0300

    Добавлен новый модуль - Наценка на выбраный способ оплаты

---
+++ lang/english/modules/order_total/ot_discountshipping.php
@@ -0,0 +1,30 @@
+<?php
+/*
+  $Id: ot_discountshipping.php,v 1.0 2003/6/19 23:09:49 wib Exp $
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+  Copyright (c) 2008 osCommerce
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_TITLE', 'Shipping Type Discount');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_DESCRIPTION', 'Shipping Type Discount');
+
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_STATUS_TITLE','Display Total');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_STATUS_DESC','Do you want to enable the Order Shipping Discount?');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_SORT_ORDER_TITLE','Sort Order');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_SORT_ORDER_DESC','Sort order of display.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_INC_SHIPPING_TITLE','Include Shipping');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_INC_SHIPPING_DESC','Include Shipping in calculation');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_INC_TAX_TITLE','Include Tax');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_INC_TAX_DESC','Include Tax in calculation.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_PERCENTAGE_TITLE','Discount Percentage');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_PERCENTAGE_DESC','Amount of Discount(percentage).');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_CALC_TAX_TITLE','Calculate Tax');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_CALC_TAX_DESC','Re-calculate Tax on discount amount.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_MINIMUM_TITLE','Minimum Amount');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_MINIMUM_DESC','Minimum order before discount');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_TYPE_TITLE','Shipping Type');
+  define('MODULE_ORDER_TOTAL_DISCOUNTSHIPPING_TYPE_DESC','Shipping Type to discount');
+  
+?>
