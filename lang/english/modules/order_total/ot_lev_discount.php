5d05a78b2bea98cf1f3c66c4dab5a7465630400c
vam <vamshop@gmail.com>
2018-12-28 20:47:53 +0300

    Добавлен новый модуль - Скидка от суммы заказа

---
+++ lang/english/modules/order_total/ot_lev_discount.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id: ot_lev_discount.php,v 1.1 2002/08/30 21:05:04 wilt Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_LEV_DISCOUNT_TITLE', 'Ваша скидка');
+  define('MODULE_LEV_DISCOUNT_DESCRIPTION', 'Ваша скидка');
+  define('SHIPPING_NOT_INCLUDED', ' [Стоимость доставки не влючена]');
+  define('TAX_NOT_INCLUDED', ' [Налог не включён]');
+?>
