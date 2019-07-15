f309f8058ffc5823fef56db68fa326d5b8e5f815
vam <vamshop@gmail.com>
2019-07-15 15:52:25 +0300

    Корректировка перевода

--- lang/english/modules/order_total/ot_lev_discount.php
+++ lang/english/modules/order_total/ot_lev_discount.php
@@ -10,8 +10,23 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_LEV_DISCOUNT_TITLE', 'Ваша скидка');
-  define('MODULE_LEV_DISCOUNT_DESCRIPTION', 'Ваша скидка');
-  define('SHIPPING_NOT_INCLUDED', ' [Стоимость доставки не влючена]');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_TITLE', 'Your Discount');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_DESCRIPTION', 'Your Discount');
+
+  define('SHIPPING_NOT_INCLUDED', ' [Стоимость доставки не включена]');
   define('TAX_NOT_INCLUDED', ' [Налог не включён]');
+
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_STATUS_TITLE', 'Показывать скидку');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_STATUS_DESC', 'Разрешить скидки?');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_SORT_ORDER_TITLE', 'Порядок сортировки');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_SORT_ORDER_DESC', 'Порядок сортировки модуля.');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_INC_SHIPPING_TITLE', 'Учитывать доставку');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_INC_SHIPPING_DESC', 'Включать в расчёт доставку.');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_INC_TAX_TITLE', 'Учитывать налог');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_INC_TAX_DESC', 'Включать в расчёт налог.');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_CALC_TAX_TITLE', 'Пересчитывать налог');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_CALC_TAX_DESC', 'Пересчитывать налог.');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_TABLE_TITLE', 'Процент скидки');
+  define('MODULE_ORDER_TOTAL_LEV_DISCOUNT_TABLE_DESC', 'Установите ценовые пределы и проценты скидки, через запятую.');
+
 ?>
