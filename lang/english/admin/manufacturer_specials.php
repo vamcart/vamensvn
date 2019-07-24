88144c6af9a39557a432418fe8c2dff672ab39ca
vam <vamshop@gmail.com>
2019-07-24 17:24:19 +0300

    Корректировка перевода

--- lang/english/admin/manufacturer_specials.php
+++ lang/english/admin/manufacturer_specials.php
@@ -14,54 +14,54 @@
    Released under the GNU General Public License
    --------------------------------------------------------------*/
 
-define('HEADING_TITLE', 'Скидки по брэндам');
+define('HEADING_TITLE', 'Special Brands');
 
-define('TABLE_HEADING_CATEGORY', 'Брэнд');
-define('TABLE_HEADING_SPECIAL_PRODUCT', 'Товар(ы)');
-define('TABLE_HEADING_CATEGORY_DISCOUNT', 'Скидка брэнда');
-define('TABLE_HEADING_STATUS', 'Статус');
-define('TABLE_HEADING_ACTION', 'Действие');
+define('TABLE_HEADING_CATEGORY', 'Category');
+define('TABLE_HEADING_SPECIAL_PRODUCT', 'Product(s)');
+define('TABLE_HEADING_CATEGORY_DISCOUNT', 'Category Discount');
+define('TABLE_HEADING_STATUS', 'Status');
+define('TABLE_HEADING_ACTION', 'Action');
 
-define('TEXT_SPECIALS_CATEGORY', 'Брэнд:');
-define('TEXT_SPECIALS_SPECIAL_PRICE', 'Специальная цена:');
-define('TEXT_SPECIALS_DISCOUNT_TYPE', 'Тип скидки:');
-define('TEXT_SPECIALS_EXPIRES_DATE', 'Действует До:');
-define('TEXT_SPECIALS_OVERRIDE', 'Переписать скидку если у товара данного брэнда уже была установлена скидка ранее.');
-define('TEXT_SPECIALS_PRICE_TIP', '<b>Примечание:</b><ul><li>Вы можете ввести процент скидки в поле "Специальная Цена", например: <b>20%</b></li><li>Если Вы вводите новую цену, десятичный разделитель должен быть \'.\' (точка), например: <b>49.99</b></li></ul>');
+define('TEXT_SPECIALS_CATEGORY', 'Category:');
+define('TEXT_SPECIALS_SPECIAL_PRICE', 'Special Price:');
+define('TEXT_SPECIALS_DISCOUNT_TYPE', 'Discount Type:');
+define('TEXT_SPECIALS_EXPIRES_DATE', 'Expiry Date:');
+define('TEXT_SPECIALS_OVERRIDE', 'Override discount of already existing Specially priced products under the category.');
+define('TEXT_SPECIALS_PRICE_TIP', '<b>Specials Notes:</b><ul><li>You can enter a percentage to deduct in the Specials Price field, for example: <b>20%</b></li><li>If you enter a new price, the decimal separator must be a \'.\' (decimal-point), example: <b>49.99</b></li><li>Leave the expiry date empty for no expiration</li></ul>');
 
-define('TEXT_INFO_DATE_ADDED', 'Дата создания:');
-define('TEXT_INFO_LAST_MODIFIED', 'Последнее изменение:');
-define('TEXT_INFO_DISCOUNT_TYPE', 'Тип скидки:');
-define('TEXT_INFO_DISCOUNT', 'Скидка:');
+define('TEXT_INFO_DATE_ADDED', 'Date Added:');
+define('TEXT_INFO_LAST_MODIFIED', 'Last Modified:');
+define('TEXT_INFO_DISCOUNT_TYPE', 'Discount Type:');
+define('TEXT_INFO_DISCOUNT', 'Discount:');
 
-define('TEXT_INFO_NEW_PRICE', 'Новая цена:');
-define('TEXT_INFO_EXPIRES_DATE', 'Действует до:');
-define('TEXT_INFO_STATUS_CHANGE', 'Статус:');
+define('TEXT_INFO_NEW_PRICE', 'New Price:');
+define('TEXT_INFO_EXPIRES_DATE', 'Expires At:');
+define('TEXT_INFO_STATUS_CHANGE', 'Status Change:');
 
-define('TEXT_INFO_HEADING_DELETE_SPECIALS', 'Удалить скидки из брэнда');
-define('TEXT_INFO_DELETE_INTRO', 'Вы действительно хотите удалить скидки из данного брэнда?');
+define('TEXT_INFO_HEADING_DELETE_SPECIALS', 'Delete Special Category');
+define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete the special category price?');
 
-define('TABLE_HEADING_PRODUCTS', 'Товары');
-define('TABLE_HEADING_PRODUCTS_PRICE', 'Цена');
-define('TABLE_HEADING_PRODUCTS_SPECIAL_PRICE', 'Цена со скидкой');
-define('TABLE_HEADING_STATUS', 'Статус');
-define('TABLE_HEADING_ACTION', 'Действие');
+define('TABLE_HEADING_PRODUCTS', 'Products');
+define('TABLE_HEADING_PRODUCTS_PRICE', 'Products Price');
+define('TABLE_HEADING_PRODUCTS_SPECIAL_PRICE', 'Specials Price');
+define('TABLE_HEADING_STATUS', 'Status');
+define('TABLE_HEADING_ACTION', 'Action');
 
-define('TEXT_INFO_DATE_ADDED', 'Дата создания:');
-define('TEXT_INFO_LAST_MODIFIED', 'Последнее изменение:');
-define('TEXT_INFO_NEW_PRICE', 'Новая цена:');
-define('TEXT_INFO_ORIGINAL_PRICE', 'Обычная цена:');
-define('TEXT_INFO_PERCENTAGE', 'Процент:');
-define('TEXT_INFO_EXPIRES_DATE', 'Действует до:');
-define('TEXT_INFO_STATUS_CHANGE', 'Статус:');
+define('TEXT_INFO_DATE_ADDED', 'Date Added:');
+define('TEXT_INFO_LAST_MODIFIED', 'Last Modified:');
+define('TEXT_INFO_NEW_PRICE', 'New Price:');
+define('TEXT_INFO_ORIGINAL_PRICE', 'Original Price:');
+define('TEXT_INFO_PERCENTAGE', 'Percentage:');
+define('TEXT_INFO_EXPIRES_DATE', 'Expires At:');
+define('TEXT_INFO_STATUS_CHANGE', 'Status Change:');
 
-define('TEXT_INFO_HEADING_DELETE_SPECIALS', 'Удалить скидку');
-define('TEXT_INFO_DELETE_INTRO', 'Вы действительно хотите удалить скидку?');
+define('TEXT_INFO_HEADING_DELETE_SPECIALS', 'Delete Special');
+define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete the special products price?');
 
-define('BUTTON_UPDATE', 'Обновить');
-define('BUTTON_RE_ADD', 'Добавить снова');
-define('BUTTON_MAKE_SPECIAL', 'Создать скидку');
-define('BUTTON_DELETE', 'Удалить');
+define('BUTTON_UPDATE', 'Update');
+define('BUTTON_RE_ADD', 'Re-Add');
+define('BUTTON_MAKE_SPECIAL', 'Make Special');
+define('BUTTON_DELETE', 'Delete');
 
-define('NEW_MANUFACTURER_DISCOUNT', 'Новая скидка по брэндам');
+define('NEW_MANUFACTURER_DISCOUNT', 'New Manufacturer Discount');
 ?>
--- lang/english/admin/specials.php
+++ lang/english/admin/specials.php
@@ -17,40 +17,35 @@
    Released under the GNU General Public License 
    --------------------------------------------------------------*/
 
-define('HEADING_TITLE', 'Скидки');
-
-define('TABLE_HEADING_PRODUCTS', 'Товары');
-define('TABLE_HEADING_PRODUCTS_PRICE', 'Цена');
-define('TABLE_HEADING_STATUS', 'Состояние');
-define('TABLE_HEADING_ACTION', 'Действие');
-
-define('TEXT_SPECIALS_PRODUCT', 'Товар:');
-define('TEXT_SPECIALS_SPECIAL_PRICE', 'Скидка:');
-define('TEXT_SPECIALS_SPECIAL_QUANTITY', 'Количество:');
-define('TEXT_SPECIALS_EXPIRES_DATE', 'Дата истекает:');
-define('TEXT_SPECIALS_PRICE_TIP', '<b>Примечание:</b><ul><li>Вы можете ввести процент скидки в поле Скидка в процентах, например: <b>20%</b></li><li>Если Вы вводите новую цену, десятичный разделитель должен быть \'.\' (десятичная-точка), например: <b>49.99</b></li></ul>');
-
-define('TEXT_INFO_DATE_ADDED', 'Дата добавления:');
-define('TEXT_INFO_LAST_MODIFIED', 'Последнее изменение:');
-define('TEXT_INFO_NEW_PRICE', 'Новая цена:');
-define('TEXT_INFO_ORIGINAL_PRICE', 'Исходная цена:');
-define('TEXT_INFO_PERCENTAGE', 'Процент:');
-define('TEXT_INFO_EXPIRES_DATE', 'Действует до:');
-define('TEXT_INFO_STATUS_CHANGE', 'Изменить статус:');
-
-define('TEXT_INFO_HEADING_DELETE_SPECIALS', 'Удалить скидку');
-define('TEXT_INFO_DELETE_INTRO', 'Вы действительно хотите удалить специальную цену для товара?');
-
-define('TEXT_IMAGE_NONEXISTENT','Нет картинки!');
+define('HEADING_TITLE', 'Specials');
+define('TABLE_HEADING_PRODUCTS', 'Products');
+define('TABLE_HEADING_PRODUCTS_PRICE', 'Products Price');
+define('TABLE_HEADING_STATUS', 'Status');
+define('TABLE_HEADING_ACTION', 'Action');
+define('TEXT_SPECIALS_PRODUCT', 'Product:');
+define('TEXT_SPECIALS_SPECIAL_PRICE', 'Special Price:');
+define('TEXT_SPECIALS_SPECIAL_QUANTITY', 'Quantity:');  
+define('TEXT_SPECIALS_EXPIRES_DATE', 'Expiry Date:');
+define('TEXT_SPECIALS_PRICE_TIP', '<b>Specials Notes:</b><ul><li>You can enter a percentage to deduct in the Specials Price field, for example: <b>20%</b></li><li>If you enter a new price, the decimal separator must be a \'.\' (decimal-point), example: <b>49.99</b></li><li>Leave the expiry date empty for no expiration</li></ul>');
+define('TEXT_INFO_DATE_ADDED', 'Date Added:');
+define('TEXT_INFO_LAST_MODIFIED', 'Last Modified:');
+define('TEXT_INFO_NEW_PRICE', 'New Price:');
+define('TEXT_INFO_ORIGINAL_PRICE', 'Original Price:');
+define('TEXT_INFO_PERCENTAGE', 'Percentage:');
+define('TEXT_INFO_EXPIRES_DATE', 'Expires At:');
+define('TEXT_INFO_STATUS_CHANGE', 'Status Change:');
+define('TEXT_INFO_HEADING_DELETE_SPECIALS', 'Delete Special');
+define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete the special products price?');
+define('TEXT_IMAGE_NONEXISTENT','No image aviable!'); 
 
 // Добавлено VaM сборка
 
-define('IMAGE_ICON_STATUS_GREEN', 'Активна');
-define('IMAGE_ICON_STATUS_GREEN_LIGHT', 'Активизировать');
-define('IMAGE_ICON_STATUS_RED', 'Неактивна');
-define('IMAGE_ICON_STATUS_RED_LIGHT', 'Сделать неактивной');
+define('IMAGE_ICON_STATUS_GREEN', 'Active');
+define('IMAGE_ICON_STATUS_GREEN_LIGHT', 'Activate');
+define('IMAGE_ICON_STATUS_RED', 'Inactive');
+define('IMAGE_ICON_STATUS_RED_LIGHT', 'Deactivate');
 
 
-define('NEW_MANUFACTURER_DISCOUNT', 'Новая скидка по брэндам');
+define('NEW_MANUFACTURER_DISCOUNT', 'New Manufacturer Discount');
 
 ?>
