41a22cf96404e3c38a46054f7952ed1f62c0b5c9
vam <vamshop@gmail.com>
2020-03-27 16:18:16 +0300

    Корректирока базы данных по умолчанию

--- install/vam.sql
+++ install/vam.sql
@@ -3261,21 +3261,21 @@ CREATE TABLE IF NOT EXISTS `specification_description` (
 ) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 
 INSERT INTO `specification_description` VALUES
-(1, 1, 1, 'Display', '', '', '', '', 'display'),
-(2, 2, 1, 'Battery', '', '', '', '', 'battery'),
-(3, 3, 1, 'Weight', '', '', '', '', 'weight'),
-(4, 4, 1, 'Storage', '', '', '', '', 'memory'),
-(5, 5, 1, 'Dimensions', '', '', '', '', 'dimensions'),
-(6, 6, 1, 'Display', '', '', '', '', 'display'),
-(7, 7, 1, 'Battery', '', '', '', '', 'battery'),
-(8, 8, 1, 'Weight', '', '', '', '', 'weight'),
-(9, 9, 1, 'Storage', '', '', '', '', 'memory'),
-(10, 10, 1, 'Dimensions', '', '', '', '', 'dimensions'),
-(11, 11, 1, 'Display', '', '', '', '', 'display'),
-(12, 12, 1, 'Battery', '', '', '', '', 'battery'),
-(13, 13, 1, 'Weight', '', '', '', '', 'weight'),
-(14, 14, 1, 'Storage', '', '', '', '', 'memory'),
-(15, 15, 1, 'Dimensions', '', '', '', '', 'dimensions');
+(1, 1, 1, 'Display', '', '', '', '', ''),
+(2, 2, 1, 'Battery', '', '', '', '', ''),
+(3, 3, 1, 'Weight', '', '', '', '', ''),
+(4, 4, 1, 'Storage', '', '', '', '', ''),
+(5, 5, 1, 'Dimensions', '', '', '', '', ''),
+(6, 6, 1, 'Display', '', '', '', '', ''),
+(7, 7, 1, 'Battery', '', '', '', '', ''),
+(8, 8, 1, 'Weight', '', '', '', '', ''),
+(9, 9, 1, 'Storage', '', '', '', '', ''),
+(10, 10, 1, 'Dimensions', '', '', '', '', ''),
+(11, 11, 1, 'Display', '', '', '', '', ''),
+(12, 12, 1, 'Battery', '', '', '', '', ''),
+(13, 13, 1, 'Weight', '', '', '', '', ''),
+(14, 14, 1, 'Storage', '', '', '', '', ''),
+(15, 15, 1, 'Dimensions', '', '', '', '', '');
 
 ##
 ## Table structure for table `specification_filters`
