3d95a25d61d2738cacaea4caaefa23a46016c6f4
vam <vamshop@gmail.com>
2020-06-15 11:04:28 +0300

    Добавлен новый виджет в админке - Кто в онлайне

--- lang/english/admin/start.php
+++ lang/english/admin/start.php
@@ -63,5 +63,19 @@
   define('TEXT_SUMMARY_TOTAL_SALES','Total Sales');
   define('TEXT_SUMMARY_TOTAL_CUSTOMERS','Total Customers');
   define('TEXT_SUMMARY_PENDING_ORDERS','Pending Orders');
+
+  define('HEADING_TITLE', 'Who\'s Online');
+
+  define('TABLE_HEADING_ONLINE', 'Online');
+  define('TABLE_HEADING_CUSTOMER_ID', 'ID');
+  define('TABLE_HEADING_FULL_NAME', 'Full Name');
+  define('TABLE_HEADING_IP_ADDRESS', 'IP Adress');
+  define('TABLE_HEADING_ENTRY_TIME', 'Entry Time');
+  define('TABLE_HEADING_LAST_CLICK', 'Last Click');
+  define('TABLE_HEADING_ACTION', 'Action');
+  define('TABLE_HEADING_LAST_PAGE_URL', 'Last URL');
+  define('TABLE_HEADING_SHOPPING_CART', 'Users Shopping Cart');
+  define('TEXT_SHOPPING_CART_SUBTOTAL', 'Subtotal');
+  define('TEXT_NUMBER_OF_CUSTOMERS', 'Currently there are %s customers online');
   
 ?>
