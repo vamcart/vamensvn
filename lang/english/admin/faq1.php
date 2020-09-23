d75df673a30ca7228d8c8119ef8286ff331379d7
vam <vamshop@gmail.com>
2020-09-23 11:21:40 +0300

    Корректирока раздела Вопросы посетителей

--- lang/english/admin/faq1.php
+++ lang/english/admin/faq1.php
@@ -14,27 +14,27 @@
    Released under the GNU General Public License 
    --------------------------------------------------------------*/
 
-define('HEADING_TITLE', 'FAQ');
+define('HEADING_TITLE', 'FAQ1');
 
-define('TABLE_HEADING_FAQ_QUESTION', 'FAQ');
-define('TABLE_HEADING_FAQ_ACTION', 'Action');
-define('TABLE_HEADING_FAQ_STATUS', 'Status');
+define('TABLE_HEADING_FAQ1_QUESTION', 'Visitors Questions');
+define('TABLE_HEADING_FAQ1_ACTION', 'Action');
+define('TABLE_HEADING_FAQ1_STATUS', 'Status');
 
-define('TEXT_FAQ_ITEMS', 'Number of faqs:');
+define('TEXT_FAQ1_ITEMS', 'Number of faqs:');
 define('TEXT_INFO_HEADING_DELETE_ITEM', 'Delete faq');
 define('TEXT_DELETE_ITEM_INTRO', 'Are you sure?');
 
-define('TEXT_FAQ_QUESTION', 'Question');
-define('TEXT_FAQ_ANSWER', 'Answer');
+define('TEXT_FAQ1_QUESTION', 'Question');
+define('TEXT_FAQ1_ANSWER', 'Answer');
 
-define('TEXT_FAQ_META_TITLE', 'Meta Title');
-define('TEXT_FAQ_META_DESCRIPTION', 'Meta Description');
-define('TEXT_FAQ_META_KEYWORDS', 'Meta Keywords');
+define('TEXT_FAQ1_META_TITLE', 'Meta Title');
+define('TEXT_FAQ1_META_DESCRIPTION', 'Meta Description');
+define('TEXT_FAQ1_META_KEYWORDS', 'Meta Keywords');
 
-define('IMAGE_NEW_FAQ_ITEM', 'Add faq');
-define('IMAGE_EDIT_FAQ_ITEM', 'Edit faq');
-define('TEXT_FAQ_LANGUAGE', 'Language');
-define('TEXT_FAQ_DATE', 'Date');
+define('IMAGE_NEW_FAQ1_ITEM', 'Add faq');
+define('IMAGE_EDIT_FAQ1_ITEM', 'Edit faq');
+define('TEXT_FAQ1_LANGUAGE', 'Language');
+define('TEXT_FAQ1_DATE', 'Date');
 
 // Сборка VaM
 
@@ -46,15 +46,15 @@ define('IMAGE_ICON_STATUS_RED_LIGHT', 'Set Inactive');
 define('EMPTY_CATEGORY', 'No faq');
 define('TEXT_NO_CHILD_CATEGORIES_OR_PRODUCTS', 'No faq at this moment');
 
-define('TEXT_FAQ_PAGE_URL', 'SEO URL FAQ');
+define('TEXT_FAQ1_PAGE_URL', 'SEO URL FAQ1');
 
-define('TEXT_FAQ_ATTACH_TO_CATEGORIES', 'Attach To Categories');
-define('TEXT_FAQ_SELECT_CATEGORIES', 'Select Categories');
+define('TEXT_FAQ1_ATTACH_TO_CATEGORIES', 'Attach To Categories');
+define('TEXT_FAQ1_SELECT_CATEGORIES', 'Select Categories');
 
-define('TEXT_FAQ_ATTACH_TO_PRODUCTS', 'Attach To Products');
-define('TEXT_FAQ_SELECT_PRODUCTS', 'Select Products');
+define('TEXT_FAQ1_ATTACH_TO_PRODUCTS', 'Attach To Products');
+define('TEXT_FAQ1_SELECT_PRODUCTS', 'Select Products');
 
-define('TEXT_FAQ_SHOW_POPULAR_PRODUCTS', 'Show 5 products from attached category');
-define('TEXT_FAQ_SHOW_DISCOUNT_PRODUCTS', 'Show 5 lowest price products from attached category');
+define('TEXT_FAQ1_SHOW_POPULAR_PRODUCTS', 'Show 5 products from attached category');
+define('TEXT_FAQ1_SHOW_DISCOUNT_PRODUCTS', 'Show 5 lowest price products from attached category');
 
 ?>
