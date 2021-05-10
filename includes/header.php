2a579b511b6ef1d84e84bb7004c50f1f3c01bcb1
vam <vamshop@gmail.com>
2021-05-10 14:48:14 +0300

    Корректировка шаблона vamshop5

--- includes/header.php
+++ includes/header.php
@@ -170,11 +170,11 @@ if (strstr($PHP_SELF, FILENAME_ACCOUNT) or strstr($PHP_SELF, FILENAME_ACCOUNT_ED
 $vamTemplate->assign('account_current',' class="current"');
 }
 
-if (strstr($PHP_SELF, FILENAME_SHOPPING_CART)) {
+if (strstr($PHP_SELF, FILENAME_SHOPPING_CART) or strstr($PHP_SELF, FILENAME_CHECKOUT) or strstr($PHP_SELF, FILENAME_CHECKOUT_SHIPPING) or strstr($PHP_SELF, FILENAME_CHECKOUT_PAYMENT) or strstr($PHP_SELF, FILENAME_CHECKOUT_CONFIRMATION) or strstr($PHP_SELF, FILENAME_CHECKOUT_SUCCESS)) {
 $vamTemplate->assign('cart_current',' class="current"');
 }
 
-if (strstr($PHP_SELF, FILENAME_WISHLIST)) {
+if (strstr($PHP_SELF, FILENAME_WISHLIST) or strstr($PHP_SELF, FILENAME_CHECKOUT) or strstr($PHP_SELF, FILENAME_CHECKOUT_SHIPPING) or strstr($PHP_SELF, FILENAME_CHECKOUT_PAYMENT) or strstr($PHP_SELF, FILENAME_CHECKOUT_CONFIRMATION) or strstr($PHP_SELF, FILENAME_CHECKOUT_SUCCESS)) {
 $vamTemplate->assign('wishlist_current',' class="current"');
 }
 
